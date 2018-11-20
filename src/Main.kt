f6a63993eba6556e37119a33ba48ecfecbbf5dc2
Tomosugi Tasaka <tuvy.ano@gmail.com>
2018-11-20 15:10:02 +0900

    add function mockData()

--- src/Main.kt
+++ src/Main.kt
@@ -6,13 +6,24 @@ import java.net.Socket
 fun main(args: Array<String>) {
     try {
         val socket = Socket(Constant.Connection.HOST_NAME, Constant.Connection.PORT_NUMBER)
-        val mainData = BlockToJson.run(
-                MainBlock(0, "hello", listOf(
-                        SubBlock.onIfInstance("true", listOf(
-                                SubBlock.ofPrintInstance())))))
+
+        val mainData = mockData()
+
         val connection = ConnectToServer(socket, mainData)
         connection.start()
     } catch (e: Exception) {
         e.printStackTrace()
     }
+}
+
+private fun mockData(): String {
+    return BlockToJson.run(
+            MainBlock(0, "hello",
+                    listOf(
+                            SubBlock.onIfInstance("true", listOf(
+                                    SubBlock.ofPrintInstance())
+                            )
+                    )
+            )
+    )
 }
