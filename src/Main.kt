f6d56b4f56dd489d2bcf1f34a82be854746db5b6
Tomosugi Tasaka <tuvy.ano@gmail.com>
2019-01-07 00:34:59 +0900

    You can call type of Int and String, Hash, Function

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     try {
         val socket = Socket(Constant.Connection.HOST_NAME, Constant.Connection.PORT_NUMBER)
 
-        val mainData = mockDataC2()
+        val mainData = mockDataJava()
 
         println(mainData)
 
@@ -22,7 +22,7 @@ private fun mockDataC(): String {
     return BlockToJson.run(
             MainBlock(0, MainBlock.LanguageType.C, "hello",
                     listOf(
-                            SubBlock.onIfInstance("true", listOf(
+                            SubBlock.ofIfInstance("true", listOf(
                                     SubBlock.ofPrintInstance())
                             )
                     )
@@ -45,6 +45,24 @@ private fun mockDataC2(): String {
             MainBlock(0, MainBlock.LanguageType.C, "hello",
                     listOf(
                             SubBlock.ofPrintInstance()
+                    )
+            )
+    )
+}
+
+private fun mockDataJava(): String {
+    return BlockToJson.run(
+            MainBlock(0, MainBlock.LanguageType.Java, "Hello",
+                    listOf(
+                            SubBlock.ofIntInstance(valueName = "a", value = "1"),
+                            SubBlock.ofIntInstance(valueName = "a", value = "2"),
+                            SubBlock.ofJavaNewIntArrayInstance(valueName = "b", values = listOf("1", "2", "3")),
+                            SubBlock.ofJavaNewIntArrayInstance(valueName = "c", size = 3),
+                            SubBlock.ofIntInstance(valueName = "a", value = "a+2"),
+                            SubBlock.ofJavaHashMapInstance(valueName = "d", type1 = "String", type2 = "String"),
+                            SubBlock.ofJavaCallFunctionInstance(functionName = "d.put", arguments = listOf("\"a\"", "\"red\"")),
+                            SubBlock.ofJavaCallFunctionInstance(valueType = "String", valueName = "e", value = "d.get(\"a\")"),
+                            SubBlock.ofPrintInstance(argument = "e")
                     )
             )
     )
--- src/model/SubBlock.kt
+++ src/model/SubBlock.kt
@@ -1,23 +1,126 @@
 package model
 
-data class SubBlock(val id: Int, val functionName: String, val arguments:List<String?>, val children: List<SubBlock?>): Block() {
+data class SubBlock(val id: Int, val functionName: String, val valueName: String?, val values: List<String?>, val arguments:List<String?>, val children: List<SubBlock?>): Block() {
 
     companion object {
         fun ofPrintInstance(argument: String = "\"Hello World\""): SubBlock {
             return SubBlock(
                     id = 0,
                     functionName = "printf",
+                    valueName = null,
+                    values = listOf(null),
                     arguments = listOf(argument),
                     children = listOf(null)
             )
         }
 
-        fun onIfInstance(argument: String, children: List<SubBlock?>): SubBlock {
+        fun ofIfInstance(argument: String, children: List<SubBlock?>): SubBlock {
             return SubBlock(
                     id = 1,
                     functionName = "if",
-                    arguments = arrayListOf(argument),
+                    valueName = null,
+                    values = listOf(null),
+                    arguments = listOf(argument),
                     children = children
+            )
+        }
+
+        fun ofWhileInstance(argument: String, children: List<SubBlock?>): SubBlock {
+            return SubBlock(
+                    id = 2,
+                    functionName = "while",
+                    valueName = null,
+                    values = listOf(null),
+                    arguments = listOf(argument),
+                    children = children
+            )
+        }
+
+        fun ofIntInstance(valueName: String, value: String): SubBlock {
+            return SubBlock(
+                    id = 3,
+                    functionName = "int",
+                    valueName = valueName,
+                    values = listOf(value),
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaStringInstance(valueName: String, value: String): SubBlock {
+            return SubBlock(
+                    id = 4,
+                    functionName = "String",
+                    valueName = valueName,
+                    values = listOf(value),
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaNewIntArrayInstance(valueName: String, values: List<String>): SubBlock {
+            return SubBlock(
+                    id = 5,
+                    functionName = "int[]",
+                    valueName = valueName,
+                    values = values,
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaNewIntArrayInstance(valueName: String, size: Int): SubBlock {
+            return SubBlock(
+                    id = 6,
+                    functionName = "int[]",
+                    valueName = valueName,
+                    values = listOf(size.toString()),
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaElementArrayInstance(valueName: String, at: Int, value: String): SubBlock {
+            return SubBlock(
+                    id = 7,
+                    functionName = "",
+                    valueName = "$valueName[$at]",
+                    values = listOf(value),
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaCallFunctionInstance(functionName: String, arguments: List<String?>): SubBlock {
+            return SubBlock(
+                    id = -1,
+                    functionName = functionName,
+                    valueName = null,
+                    values = listOf(null),
+                    arguments = arguments,
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaCallFunctionInstance(valueType: String, valueName: String, value: String): SubBlock {
+            return SubBlock(
+                    id = -1,
+                    functionName = valueType,
+                    valueName = valueName,
+                    values = listOf(value),
+                    arguments = listOf(null),
+                    children = listOf(null)
+            )
+        }
+
+        fun ofJavaHashMapInstance(valueName: String, type1: String, type2: String): SubBlock {
+            return SubBlock(
+                    id = 8,
+                    functionName = "Map<$type1,$type2>",
+                    valueName = valueName,
+                    values = listOf(null),
+                    arguments = listOf(null),
+                    children = listOf(null)
             )
         }
     }
